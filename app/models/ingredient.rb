55af9564c2d52b9c401cb2b94e3a522dabae603f
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-07-28 19:15:53 -0400

    form updated

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,18 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 
-  # def ingredient_ids=(ingredient_ids)
-  #   i = Ingredient.find(ingredient_ids)
-  #   @recipe.ingredients << i
-  # end
-
-
-
-  # def ingredients_attributes=(ingredients_attributes)
-  #   @recipe.ingredients.each do |ingredient|
-  #     @recipe.ingredients << ingredient.name
-  #   end
-  # end
-
 
 end
